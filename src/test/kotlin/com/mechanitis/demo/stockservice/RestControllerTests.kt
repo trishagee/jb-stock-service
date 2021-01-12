e39a8702854dde572cd408f41a6a60cc1e9ae0f3
Trisha Gee <trisha.gee@gmail.com>
2021-01-12 13:28:31 +0100

    Trying out the tests from PR-1

---
+++ src/test/kotlin/com/mechanitis/demo/stockservice/RestControllerTests.kt
@@ -0,0 +1,40 @@
+package com.mechanitis.demo.stockservice
+
+import org.junit.jupiter.api.Test
+import org.mockito.BDDMockito.given
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.test.web.reactive.server.returnResult
+import reactor.core.publisher.Flux
+import reactor.kotlin.test.test
+import java.time.LocalDateTime.now
+
+@WebFluxTest(controllers = [RestController::class])
+class RestControllerTests(@Autowired val client: WebTestClient) {
+    @MockBean
+    private lateinit var priceService: PriceService
+
+    @Test
+    fun shouldReceiveStockPricesForSymbolViaGET() {
+        given(
+            priceService.generatePrices("SYMB")
+        ).willReturn(
+            Flux.just(
+                StockPrice("SYMB", 1.3, now())
+            )
+        )
+
+        val stockPrices = client.get().uri("/stocks/SYMB").exchange()
+            .expectStatus().isOk
+            .returnResult<StockPrice>().responseBody
+
+        stockPrices.test()
+            .expectNextMatches {
+                it.symbol == "SYMB" &&
+                        it.price == 1.3
+            }
+            .verifyComplete()
+    }
+}
--- src/test/kotlin/com/mechanitis/demo/stockservice/StockServiceApplicationTests.kt
+++ src/test/kotlin/com/mechanitis/demo/stockservice/StockServiceApplicationTests.kt
@@ -1,13 +1,78 @@
 package com.mechanitis.demo.stockservice
 
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.messaging.rsocket.RSocketRequester
+import reactor.core.publisher.Flux
+import reactor.test.StepVerifier
+import java.time.Duration.ofSeconds
+import org.xmlunit.builder.Transform.source
+import java.util.function.Predicate
+
 
 @SpringBootTest
+@TestInstance(PER_CLASS)
 class StockServiceApplicationTests {
+    var requester: RSocketRequester? = null
+
+    @BeforeAll
+    fun setupOnce(
+        @Autowired builder: RSocketRequester.Builder,
+        @Value("\${spring.rsocket.server.port}") port: Int
+    ) {
+        requester = builder.tcp("localhost", port)
+    }
 
     @Test
     fun contextLoads() {
     }
 
+    @Test
+    fun testRequestGetsResponse() {
+        StepVerifier
+            .create(requester!!
+                    .route("stockPrices")
+                    .data("SYMB")
+                    .retrieveFlux(StockPrice::class.java).take(1))
+            .expectNextMatches {
+                it.symbol == "SYMB" && it.price != 0.0
+            }
+            .expectComplete()
+            .verify()
+
+    //        StepVerifier
+//            .withVirtualTime {
+//                requester!!
+//                    .route("stockPrices")
+//                    .data("a")
+//                    .retrieveFlux(StockPrice::class.java).take(1)
+//            }
+//            .expectSubscription()
+//            .expectNextMatches {
+//                it.symbol == "a"
+//            }
+//            .verifyComplete()
+
+    }
+
+    @Test
+    internal fun testStepVerifierWithVirtualTime() {
+        StepVerifier
+            .withVirtualTime {
+                Flux.interval(
+                    ofSeconds(1)
+                ).take(2)
+            }
+            .expectSubscription()
+            .expectNoEvent(ofSeconds(1))
+            .expectNext(0L)
+            .thenAwait(ofSeconds(1))
+            .expectNext(1L)
+            .verifyComplete()
+    }
 }
